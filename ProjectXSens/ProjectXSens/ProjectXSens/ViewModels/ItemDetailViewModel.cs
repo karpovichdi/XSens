0c6c32dadcd567bf6282db2c625708c1733b8b2d
Karpovich Dmitry <d.karpovich@godeltech.com>
2020-06-07 15:29:35 +0300

    Added: Init version of project with gitignore.

---
+++ ProjectXSens/ProjectXSens/ProjectXSens/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ProjectXSens.Models;
+
+namespace ProjectXSens.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
